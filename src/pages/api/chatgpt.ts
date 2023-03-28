37fb136d11062d1a63db3b7be74a2b4be8c75c7b
ricky zhao <rizhao1124@gmail.com>
2023-03-28 15:02:13 -0700

    feat: add prompt construction

--- src/pages/api/chatgpt.ts
+++ src/pages/api/chatgpt.ts
@@ -1,16 +1,36 @@
 import { Configuration, OpenAIApi } from "openai";
+import {topics} from '../../constants/topic';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+const constructPrompt = (topic: string, prompt='', language = 'javascript', level = 'junior', position = 'full-stack engineer') => {
+  switch(topic) {
+    case topics.EXPLAIN_CODE:
+      return `Explain this block of code: ${prompt}`;
+    case topics.FIX_CODE:
+      return `Is there anything wrong this block of code: ${prompt}`;
+    case topics.WRITE_CODE:
+      return `Write Some Code that does the following: ${prompt}`;
+    case topics.INTERVIEW_QUESTION:
+      return `Give me a list of interview questions for a ${level} ${position}.`
+    case topics.CODING_QUESTION:
+      return `Give me a list of ${level} coding questions in ${language}`
+    case topics.CODING_QUESTION_ANSWER:
+      return `Give me the answers to the following questions: ${prompt}`
+    default:
+      return prompt;
+  }
+}
 export default async function chatGpt(req, res) {
+  const prompt = constructPrompt(topics.WRITE_CODE, req.body.prompt);
   const completion = await openai.createChatCompletion({
     messages: [
         {
-            role: "system",
-            content: req.body.prompt + '\n\n###\n\n',
+            role: "user",
+            content: prompt + '\n\n###\n\n',
         }
     ],
     temperature: 0.7,
@@ -20,6 +40,6 @@ export default async function chatGpt(req, res) {
     presence_penalty: 0,
     model: "gpt-3.5-turbo",
 });
-
-  res.status(200).json({ result: completion?.data?.choices[0].message?.content });
+  const response = completion?.data?.choices[0]?.message?.content;
+  res.status(200).json({ result: response });
 }
