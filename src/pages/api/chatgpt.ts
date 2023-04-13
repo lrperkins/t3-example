af570777533d9dbdad3c0b2bfb2bda4ca086be69
Levi Perkins <levi.r.perkins@gmail.com>
2023-04-13 09:20:26 -0400

    fix(api-ai-documentation-fail)

--- src/pages/api/chatgpt.ts
+++ src/pages/api/chatgpt.ts
@@ -1,39 +1,42 @@
-// Create a new OpenAI API client
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-// Retrieve the user messages from the request body
-const messages = req.body;
+export default async function chatGpt(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const messages = req.body;
 
-if (!messages || !Array.isArray(messages)) {
-  res.status(400).json({ error: "Invalid request: messages should be an array" });
-  return;
-}
+  if (!messages || !Array.isArray(messages)) {
+    res.status(400).json({ error: "Invalid request: messages should be an array" });
+    return;
+  }
 
-// Format the user messages
-const openaiMessages = messages.map((message) => ({
-  role: message.role,
-  content: message.content,
-}));
+  const openaiMessages = messages.map((message) => ({
+    role: message.role,
+    content: message.content,
+  }));
 
-// Call the OpenAI API to generate a response
-try {
-  const completion = await openai.createChatCompletion({
-    messages: openaiMessages,
-    temperature: 0.7,
-    max_tokens: 1500,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    model: "gpt-3.5-turbo",
-  });
+  try {
+    const completion = await openai.createChatCompletion({
+      messages: openaiMessages,
+      temperature: 0.7,
+      max_tokens: 1500,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      model: "gpt-3.5-turbo",
+    });
 
-  // Send the response back to the client
-  const response = completion?.data?.choices[0]?.message?.content;
-  res.status(200).json({ role: "assistant", content: response });
-} catch (error) {
-  console.error(error);
-  res.status(500).json({ error: "An error occurred while processing the request" });
+    const response = completion?.data?.choices[0]?.message?.content;
+    res.status(200).json({ role: "assistant", content: response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while processing the request" });
+  }
 }
